cf4c4da0a86be0329765c72025e8e35cc32d2904
user0208308930298929832 <miguelbretiano@gmail.com>
2025-11-16 20:10:40 +0000

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "CreatorAgent",
+  description: "Landing Page",
+};
+
+export default function RootLayout({ children }: any) {
+  return (
+    <html lang="pt">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
