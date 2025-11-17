aff512a64b64cb0ffb9a8479eab9fe20db1511e2
user0208308930298929832 <miguelbretiano@gmail.com>
2025-11-17 12:30:34 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}"
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#3b82f6",
-        dark: "#0f1115",
-        darker: "#0b0c10",
-        card: "#15171c"
-      }
-    }
+    extend: {},
   },
   plugins: [],
 };
